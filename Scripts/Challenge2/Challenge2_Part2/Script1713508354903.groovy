be8bd11c7cbeb5bdeb6c8899db1953753e058fe5
Nicolas Fontes de Mello <nicolasfontesdemello@hotmail.com>
2024-04-23 12:31:19 -0400

    Challenge2 part 2 and part3

--- Scripts/Challenge2/Challenge2_Part2/Script1713508354903.groovy
+++ Scripts/Challenge2/Challenge2_Part2/Script1713508354903.groovy
@@ -12,10 +12,16 @@ import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.By as By
+
+
 
 WebUI.callTestCase(findTestCase('Challenge/LoginTest'), [:], FailureHandling.STOP_ON_FAILURE)
 
@@ -27,13 +33,38 @@ WebUI.click(findTestObject('getJobsElement/Page_Positions - MissionNext.org/jour
 
 WebUI.switchToWindowTitle('Journey Missionary Job Summary - MissionNext.org')
 
-WebUI.click(findTestObject('Object Repository/challenge2/Page_Journey Missionary Job Summary - Missi_d1fec4/span_View Job Details by Category'))
 
-data1 = WebUI.getText(findTestObject('evangelismObj/Page_Journey missionary-jobs-detail - MissionNext.org/td_EVANGELISM (72)'))
+WebDriver driver = DriverFactory.getWebDriver()
 
-data2 = WebUI.getText(findTestObject('evangelismObj/Page_Journey missionary-jobs-detail - MissionNext.org/td_EVANGELISM SUPPORT (16)'))
+String ExpectedValue = "EVANGELISM";
 
-System.out.println(data1+ " + "+data2+" = "+ 88)
+WebElement Table = driver.findElement(By.xpath("//table[1]/tbody[1]"))
+
+List<WebElement> rows_table = Table.findElements(By.tagName('tr'))
+int rows_count = rows_table.size()
+int jobCount = 0
+ 
+Loop:
+for (int row = 0; row < rows_count; row++) {
+
+List<WebElement> Columns_row = rows_table.get(row).findElements(By.tagName('td')) 
+int columns_count = Columns_row.size()
+println((('Number of cells In Row ' + row) + ' are ') + columns_count)
+
+
+for (int column = 0; column < columns_count; column++) {
+
+String celltext = Columns_row.get(column).getText()
+ 
+
+if (celltext == ExpectedValue) {
+'Getting the count up by 1 if cell text i.e Company name matches with Expected value'
+	jobCount++
+}
+}
+}
+
+System.out.println("The number of job avai;able for both categories of Evangelism is: "+ jobCount)
 
 WebUI.closeBrowser()
 
